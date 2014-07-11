b745505085619d2818bc4b32b5a9fcc539e551fd
Brad Fitzpatrick <brad@danga.com>
2014-07-11 16:11:57 -0700

    Add missing copyright headers. Make TestAllPixels more visible.

--- gen_test.go
+++ gen_test.go
@@ -1,5 +1,21 @@
 // +build latlong_gen
 
+/*
+Copyright 2014 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package latlong
 
 import (
@@ -189,7 +205,11 @@ func (s *setIndexTracker) Add(v interface{}) (idx uint16, isNew bool) {
 	return idx, true
 }
 
-func TestAllPixels(t *testing.T) {
+func init() {
+	testAllPixels = testAllPixels_gen
+}
+
+func testAllPixels_gen(t *testing.T) {
 	if degPixels == -1 {
 		t.Skip("data not generated yet")
 	}
--- latlong_test.go
+++ latlong_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2014 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package latlong
 
 import "testing"
@@ -14,6 +30,15 @@ func TestLookupLatLong(t *testing.T) {
 			t.Errorf("LookupZoneName(%v, %v) = %q; want %q", tt.lat, tt.long, got, tt.want)
 		}
 	}
+}
+
+var testAllPixels func(t *testing.T)
+
+func TestAllPixels(t *testing.T) {
+	if testAllPixels == nil {
+		t.Skip("exhaustive pixel test disabled without --tags=latlong_gen (requires extra deps)")
+	}
+	testAllPixels(t)
 }
 
 func TestLookupPixel(t *testing.T) {
